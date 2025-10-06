a902eefce06d101c1cad93b60d23673ef8e8bfc5
Abhijeet Shashwat <asp616848@gmail.com>
2025-10-06 21:54:14 +0530

    remove

--- assighment_2/code/run_all.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-python main.py ../EUCLIDEAN_50.txt output_E50.txt
-python main.py ../EUCLIDEAN_100.txt output_E100.txt
-python main.py ../EUCLIDEAN_200.txt output_E200.txt
-python main.py ../NON_EUCLIDEAN_50.txt output_NE50.txt
-python main.py ../NON_EUCLIDEAN_100.txt output_NE100.txt
-python main.py ../NON_EUCLIDEAN_200.txt output_NE200.txt
